e54f44ff072eb28ef242a6dcdf7d0d1e01d5db0e
AZ <zalexandra994@gmail.com>
2022-12-21 12:56:17 +0100

    edit

--- pobierz dane programisty/js/script.js
+++ pobierz dane programisty/js/script.js
@@ -9,9 +9,12 @@ $(document).ready(function () {
         
         // console.log(JSON.parse(data));
 
+        let dataObject = JSON.parse(data);
 
             $("#dane-programisty").append(
                 "<br> Imię: " + data.imie, 
+                // JSON.parse(data).imie
+                // dataObject.imie
                 "<br> Nazwisko: " + data.nazwisko, 
                 "<br> Zawód: " + data.zawod, 
                 "<br> Firma: " + data.firma, 
@@ -24,23 +27,5 @@ $(document).ready(function () {
 
 })
 
-// $(document).ready(function () {
 
-//     $("body").append('<div id="dane-programisty">data</div>');
-
-//     function clickButton(e) {
-//         e.preventDefault
-
-     
-//         $.get("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
-//             let dane = data;
-//             console.log(dane);
-        
-//             $("#dane-programisty").text(dane);
-//         });
-        
-//     }
-//     $("#btn").on("click", clickButton);
-
-// })
 
